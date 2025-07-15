082862ddaedcd5819fb0801fb4dd5f73e5e465f5
JoaquimPinto <JoaquimPinto@DESKTOP-7HOJ4UO>
2025-07-15 12:02:33 +0100

    Adicionar arquivos de projeto.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Biblioteca1.Startup))]
+namespace Biblioteca1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
